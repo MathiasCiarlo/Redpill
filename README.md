723c627cb621f0d3ad8f4fe4eb0df135ec189bb0
Mathias Ciarlo Thorstensen <mathiact@gmail.com>
2015-06-08 22:51:46 +0200

    Fixed the issues. Added manual to README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Redpill - A circular pill dispencer
+# Redpill - An automatic circular pill dispencer
 Aruino project for the course "inf1510" at UiO.
+
+##Manual
+###Red Pill consists of:
+- Rotating disc with 28 pill chambers
+- 4 switches to control which times to dipsose pills
+- 1 button to control the pill hatch
+- 1 LED
+
+###How it works
+The dispenser rotates each preset time, for testing puposes, theese are set to 2, 10, 18, 26 seconds. The clock resets at 32 secs. The switches on the side of the dispencer control which of the times a pill can be dispenced. When the dispencer rotates to a time corresponding to a switch in **ON** position, the LED will turn **green** and the button is activated. When the button is pressed a hatch opens and the pills falls into a shelf beneath. The shelf can be extracted.
+
+###Details
+Green LED means a dose of pills is ready and the hatch can open to serve it.
+Blue LED means the there are no current dose ready, but the system is waiting for the next serving time. The button is disabled.
+LED off means all switches are in **OFF** position and the button is disabled.
+
+###Code
+Some lines are marked blocking. This means the code has a delay built in.
--- Redpill.ino
+++ Redpill.ino
@@ -43,12 +43,14 @@ int switchValues[4];
 
 // Initialization, set current state to last state
 int currentState = 3;
-int nextRotaionTime = -1;
+int lockedState = 3;
 
 int prevSec = -1;
 int currSec = 0;
+int resetTime; // DEBUG
 
 boolean readyToServe = false;
+boolean longRotation = true;
 
 time_t startTime; // Debug (To reset time)
 
@@ -74,10 +76,11 @@ void setup() {
     // 80000, 120000, 160000, 200000
     // However for testing purposes, they are set
     // within the first minute after midnight.
-    pillTimes[0] = 5;
+    pillTimes[0] = 2;
     pillTimes[1] = 10;
-    pillTimes[2] = 15;
-    pillTimes[3] = 20;
+    pillTimes[2] = 18;
+    pillTimes[3] = 26;
+    resetTime = 32;
 
     switchValues[0] = HIGH;
     switchValues[1] = HIGH;
@@ -85,10 +88,6 @@ void setup() {
     switchValues[3] = HIGH;
 
     startTime = now(); // Debug
-
-    digitalWrite(3, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(1, LOW);
 }
 
 void loop() {
@@ -100,32 +99,42 @@ void loop() {
         updateSwitchValues();
         if (currSec % 5 == 0) printSwitchValues(); // DEBUG
 
-        // Check if there is time for the next chamber.
-        // If so, rotate, turn on green LED,
-        // set ready to serve
+	// If all switches are off, nothing will occur.
+        int nextActiveState = getNextActiveState();
 
-        // If all switches are off, nothing will occur.
-        int nextState = getNextState();
+        if (nextActiveState == -1)
+            digitalWrite(ledPinB, LOW);
+        else
+            digitalWrite(ledPinB, HIGH);
 
-        if (nextState != -1) {
-            if (timeForNextState(nextState)) {
-                digitalWrite(ledPinB, LOW);
-                setState(nextState);
-                rotateToNext();
+	// Check if there is time for next state
+	// If so, we rotate
+        if (timeForNextState()){
+            goToNextState();
+	    rotateSteps(1);
+            delay(500);
+
+            // If the new state is an active state (the switch is ON
+            // for this state) we rotate to it.
+            if (currentState == nextActiveState && currentState != lockedState) {
                 readyToServe = true;
+                digitalWrite(ledPinB, LOW);
                 digitalWrite(ledPinG, HIGH);
                 print("Ringing alarm.. Push the button to dispence!");
             }
+
+            // Lock the current state
+            lockedState = currentState;
         }
 
-        prevSec = currSec;
-        print("");
+        String status = "State: " + String(currentState);
+        print(status);
 
-        // Reset at 45 seconds beyond midnight for testing purposes.
-        if (currSec >= 25) {
+        // Reset at 25 seconds beyond midnight for testing purposes.
+        if (currSec >= resetTime) {
             setTime(startTime);
-            readyToServe = false;
         }
+        prevSec = currSec;
     }
 
     // Check if the button is pressed and we are ready to serve
@@ -141,15 +150,17 @@ void loop() {
 }
 
 
-// Returns true if there is a pill to serve
+// Returns true if we have entered next state
 // Input: int (0-3)
-boolean timeForNextState(int nextState) {
+boolean timeForNextState() {
+    int nextState = (currentState == 3) ? 0 : currentState + 1;
 
-    // Special code if only one switch is ON.
-    if (currentState == nextState)
-	return false;
-	
-    int nextRotationTime = pillTimes[nextState];
+    // If only one state is activated, we need to avoid to enter it again,
+    // since the next state will be the same.
+    if (nextState == lockedState)
+        return false;
+
+    int nextStateTime = pillTimes[nextState];
 
     // Converting the current time to an int that increases
     // each second through the the day
@@ -157,21 +168,23 @@ boolean timeForNextState(int nextState) {
     int timeInt = timeString.toInt();
 
     // Check if we are in state EVENING (last state) and time is less than midnight
-    // Need special code for this situation, since nextRotationTime is less than
+    // Need special code for this situation, since nextStateTime is less than
     // current time.
-    
+
     if (currentState == 3 && timeInt <= 235959 && timeInt > pillTimes[3]) {
         return false;
     }
 
-    int nextNextState = nextState + 1;
-    if (nextNextState > 3)
-	nextNextState -= 4;
+    /*
+      int nextNextState = nextState + 1;
+      if (nextNextState > 3)
+      nextNextState -= 4;
 
-    int nextNextRotationTime = pillTimes[nextNextState];
+      int nextNextRotationTime = pillTimes[nextNextState];
+    */
 
     // Check if have entered the next time state
-    if (timeInt >= nextRotationTime && timeInt < nextNextRotationTime)
+    if (timeInt >= nextStateTime)// && timeInt < nextNextRotationTime)
         return true;
 
     return false;
@@ -189,7 +202,7 @@ String getTimeString() {
 
 // Returns the next state by checking current state and switch values
 // Return -1 if all switches are off.
-int getNextState() {
+int getNextActiveState() {
 
     // Loop trough the states {0, 1, 2, 3} as a ring, from current state.
     for (int i = 0; i < 4; i++) {
@@ -204,43 +217,38 @@ int getNextState() {
     return -1;
 }
 
-// Changes state to next valid state.
-void setState(int state) {
+// Changes state to next state.
+void goToNextState() {
+    if (currentState == 3)
+        currentState = 0;
+    else
+        currentState++;
 
-    switch(state) {
+    switch(currentState) {
     case 0:
-        print("State MORNING");
+        print("Entered state MORNING");
         break;
     case 1:
-        print("State NOON");
+        print("Entered state NOON");
         break;
     case 2:
-        print("State AFTERNOON");
+        print("Entered state AFTERNOON");
         break;
     case 3:
-        print("State EVENING");
+        print("Entered state EVENING");
         break;
     }
-
-    currentState = state;
 }
 
-// Calculates the correct number of steps and rotates.
-void rotateToNext() {
-    int steps = 1;
-    int debugCnt = 0;
-
-    String tmp = "Rotating " + String(steps) + " steps..";
-    print(tmp);
-
-    rotateSteps(steps);
-
-    print("Rotation complete. Ready to dispence pills.");
-}
 
 // Rotates the wheel a number of steps
 void rotateSteps(int steps) {
-    steps = steps * -29;
+    int scale = longRotation ? -29 : -28;
+    longRotation = !longRotation;
+
+    print("Rotating");
+    
+    steps = steps * scale;
 
     // Blocking line, waits until rotation is complete
     stepperMotor.step(steps);
@@ -274,19 +282,18 @@ void setPinModes() {
     pinMode(switch2Pin, INPUT);
     pinMode(switch3Pin, INPUT);
     pinMode(switch4Pin, INPUT);
+
     pinMode(buttonPin, INPUT);
 
     pinMode(ledPinB, OUTPUT);
     pinMode(ledPinG, OUTPUT);
-    //pinMode(ledPinR, OUTPUT); // Deaktivert
-    pinMode(hatchPin, OUTPUT);
 
     pinMode(stepper1Pin, OUTPUT);
     pinMode(stepper2Pin, OUTPUT);
     pinMode(stepper3Pin, OUTPUT);
     pinMode(stepper4Pin, OUTPUT);
 
-    //pinMode(hatchPin, OUTPUT);
+    pinMode(hatchPin, OUTPUT);
 }
 
 void updateSwitchValues() {
